b50359a63b7f99dee12eae5a4509b432ff1db9a7
EdwardOliveros <esoliveros60@soy.sena.edu.co>
2023-08-11 14:59:06 -0500

    Buscador

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ btnSignUp.addEventListener("click", e =>{
     formLogin.classList.add("hide");
     formResgister.classList.remove("hide");
 })
+
+
+
+bars_search = document.getElementById("container-bars-search");
+cover_container_search = document.getElementById("cover-container-search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementsById("box-search");
+
+//FUNCION PARA MOSTRAR EL BUSCADOR
+
